c9ef4ef5d8c831947d7c3ba87e7862df1c177174
hanneslindberg <hannes.lindberg@elev.ga.ntig.se>
2024-03-19 21:18:05 +0100

    19/03 v.2

--- js/main.js
+++ js/main.js
@@ -3,56 +3,70 @@ let movies = [
     name: "Kung Fu Panda",
     rating: 7.6,
     actors: ["Jack Black", "Angelina Jolie"],
-    description: "To everyone's surprise, including his own, Po, an overweight, clumsy panda, is chosen as protector of the Valley of Peace. His suitability will soon be tested as the valley's arch-enemy is on his way."
+    description:
+      "To everyone's surprise, including his own, Po, an overweight, clumsy panda, is chosen as protector of the Valley of Peace. His suitability will soon be tested as the valley's arch-enemy is on his way.",
   },
   {
     name: "Titanic",
     rating: 7.9,
     actors: ["Leonardo Giannini", "Hannes Winslet"],
-    description: "A seventeen-year-old aristocrat falls in love with a kind but poor artist aboard the luxurious, ill-fated R.M.S. Titanic."
+    description:
+      "A seventeen-year-old aristocrat falls in love with a kind but poor artist aboard the luxurious, ill-fated R.M.S. Titanic.",
   },
   {
     name: "Interstellar",
     rating: 8.7,
     actors: ["Matthew McConaughey", "Anne Hathaway", "Jessica Chastain"],
-    description: "When Earth becomes uninhabitable in the future, a farmer and ex-NASA pilot, Joseph Cooper, is tasked to pilot a spacecraft, along with a team of researchers, to find a new planet for humans."
+    description:
+      "When Earth becomes uninhabitable in the future, a farmer and ex-NASA pilot, Joseph Cooper, is tasked to pilot a spacecraft, along with a team of researchers, to find a new planet for humans.",
   },
   {
     name: "Dune: Part Two",
     rating: 8.9,
     actors: ["Timothée Chalamet", "Zendaya"],
-    description: "Paul Atreides unites with Chani and the Fremen while seeking revenge against the conspirators who destroyed his family."
+    description:
+      "Paul Atreides unites with Chani and the Fremen while seeking revenge against the conspirators who destroyed his family.",
   },
   {
     name: "Oppenheimer",
     rating: 8.4,
-    actors: ["Cillian Murphy", "Emily Blunt", "Matt Damon", "Robert Downey Junior"],
-    description: "The story of American scientist J. Robert Oppenheimer and his role in the development of the atomic bomb."
+    actors: [
+      "Cillian Murphy",
+      "Emily Blunt",
+      "Matt Damon",
+      "Robert Downey Junior",
+    ],
+    description:
+      "The story of American scientist J. Robert Oppenheimer and his role in the development of the atomic bomb.",
   },
   {
     name: "Avatar",
     rating: 7.9,
     actors: ["Sam Worthington", "Zoe Saldana"],
-    description: "A paraplegic Marine dispatched to the moon Pandora on a unique mission becomes torn between following his orders and protecting the world he feels is his home."
+    description:
+      "A paraplegic Marine dispatched to the moon Pandora on a unique mission becomes torn between following his orders and protecting the world he feels is his home.",
   },
   {
     name: "The Shining",
     rating: 8.4,
     actors: ["Jack Nicholson", "Shelly Duval"],
-    description: "A family heads to an isolated hotel for the winter where a sinister presence influences the father into violence, while his psychic son sees horrific forebodings from both past and future."
+    description:
+      "A family heads to an isolated hotel for the winter where a sinister presence influences the father into violence, while his psychic son sees horrific forebodings from both past and future.",
   },
   {
     name: "Next",
     rating: 6.2,
     actors: ["Jicholas Cage", "Julianne Moore", "Jessica Biel"],
-    description: "A Las Vegas magician who can see into the future is pursued by FBI agents seeking to use his abilities to prevent a nuclear terrorist attack."
-  }, 
+    description:
+      "A Las Vegas magician who can see into the future is pursued by FBI agents seeking to use his abilities to prevent a nuclear terrorist attack.",
+  },
   {
     name: "Fight Club",
     rating: 8.8,
     actors: ["Brad Pitt", "Edward Norton", "Helena Bonham Carter"],
-    description: "An insomniac office worker and a devil-may-care soap maker form an underground fight club that evolves into much more."
-  }
+    description:
+      "An insomniac office worker and a devil-may-care soap maker form an underground fight club that evolves into much more.",
+  },
 ];
 
 let template = document.getElementById("movies");
@@ -66,9 +80,6 @@ for (let movie of movies) {
 
   template.parentElement.appendChild(copy);
 }
-
-console.log(movies[0].actors[0]);
-console.log(movies[0].actors[1]);
 
 document.addEventListener("DOMContentLoaded", function () {
   // Hämta referens till alla länkar för filmer
@@ -84,9 +95,7 @@ document.addEventListener("DOMContentLoaded", function () {
       let movieName = this.querySelector(".name").textContent;
 
       // Skapa en URL-parametersträng med den sparade informationen
-      let queryParams =
-        "?name=" +
-        encodeURIComponent(movieName) 
+      let queryParams = "?name=" + encodeURIComponent(movieName);
 
       // Skicka användaren till den andra sidan med URL-parametrarna
       window.location.href = "filmer.html" + queryParams;
