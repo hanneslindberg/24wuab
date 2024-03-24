c4093c6e34d204dd1fa48dc2442b418abac0d44f
hanneslindberg <hannes.lindberg@elev.ga.ntig.se>
2024-03-24 21:11:23 +0100

    final commit

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@ let movies = [
     actors: ["Jack Black", "Angelina Jolie"],
     description:
       "To everyone's surprise, including his own, Po, an overweight, clumsy panda, is chosen as protector of the Valley of Peace. His suitability will soon be tested as the valley's arch-enemy is on his way.",
+    image: "/img/kung-fu-panda.jpg",
   },
   {
     name: "Titanic",
@@ -12,6 +13,7 @@ let movies = [
     actors: ["Leonardo Giannini", "Hannes Winslet"],
     description:
       "A seventeen-year-old aristocrat falls in love with a kind but poor artist aboard the luxurious, ill-fated R.M.S. Titanic.",
+    image: "/img/titanic.jpg",
   },
   {
     name: "Interstellar",
@@ -19,6 +21,7 @@ let movies = [
     actors: ["Matthew McConaughey", "Anne Hathaway", "Jessica Chastain"],
     description:
       "When Earth becomes uninhabitable in the future, a farmer and ex-NASA pilot, Joseph Cooper, is tasked to pilot a spacecraft, along with a team of researchers, to find a new planet for humans.",
+    image: "/img/interstellar.jpg",
   },
   {
     name: "Dune: Part Two",
@@ -26,6 +29,7 @@ let movies = [
     actors: ["Timothée Chalamet", "Zendaya"],
     description:
       "Paul Atreides unites with Chani and the Fremen while seeking revenge against the conspirators who destroyed his family.",
+    image: "/img/dune-part-two.jpg",
   },
   {
     name: "Oppenheimer",
@@ -38,6 +42,7 @@ let movies = [
     ],
     description:
       "The story of American scientist J. Robert Oppenheimer and his role in the development of the atomic bomb.",
+    image: "/img/oppenheimer.jpg",
   },
   {
     name: "Avatar",
@@ -45,6 +50,7 @@ let movies = [
     actors: ["Sam Worthington", "Zoe Saldana"],
     description:
       "A paraplegic Marine dispatched to the moon Pandora on a unique mission becomes torn between following his orders and protecting the world he feels is his home.",
+    image: "/img/avatar.jpg",
   },
   {
     name: "The Shining",
@@ -52,6 +58,7 @@ let movies = [
     actors: ["Jack Nicholson", "Shelly Duval"],
     description:
       "A family heads to an isolated hotel for the winter where a sinister presence influences the father into violence, while his psychic son sees horrific forebodings from both past and future.",
+    image: "/img/the-shining.jpg",
   },
   {
     name: "Next",
@@ -59,6 +66,7 @@ let movies = [
     actors: ["Jicholas Cage", "Julianne Moore", "Jessica Biel"],
     description:
       "A Las Vegas magician who can see into the future is pursued by FBI agents seeking to use his abilities to prevent a nuclear terrorist attack.",
+    image: "/img/next.jpg",
   },
   {
     name: "Fight Club",
@@ -66,6 +74,20 @@ let movies = [
     actors: ["Brad Pitt", "Edward Norton", "Helena Bonham Carter"],
     description:
       "An insomniac office worker and a devil-may-care soap maker form an underground fight club that evolves into much more.",
+    image: "/img/fight-club.jpg",
+  },
+  {
+    name: "Pulp Fiction",
+    rating: 8.9,
+    actors: [
+      "John Travolta",
+      "Uma Thurman",
+      "Samuel L. Jackson",
+      "Bruce Willis",
+    ],
+    description:
+      "The lives of two mob hitmen, a boxer, a gangster and his wife, and a pair of diner bandits intertwine in four tales of violence and redemption.",
+    image: "/img/pulp-fiction.jpg",
   },
 ];
 
@@ -76,28 +98,22 @@ for (let movie of movies) {
 
   copy.querySelector(".name").textContent = movie.name;
   copy.querySelector(".rating").textContent = movie.rating;
-  copy.querySelector(".actors").textContent = movie.actors.join(" - ");
+  copy.querySelector(".image").src = movie.image;
 
   template.parentElement.appendChild(copy);
 }
 
 document.addEventListener("DOMContentLoaded", function () {
-  // Hämta referens till alla länkar för filmer
   let movieLinks = document.querySelectorAll(".link_movie");
 
-  // Lägg till eventlyssnare för varje länk
   movieLinks.forEach(function (link) {
     link.addEventListener("click", function (event) {
-      // Förhindra standardbeteende för länken
       event.preventDefault();
 
-      // Hämta information om den klickade filmen från dess förälderelement
       let movieName = this.querySelector(".name").textContent;
 
-      // Skapa en URL-parametersträng med den sparade informationen
       let queryParams = "?name=" + encodeURIComponent(movieName);
 
-      // Skicka användaren till den andra sidan med URL-parametrarna
       window.location.href = "filmer.html" + queryParams;
     });
   });
--- js/page2.js
+++ js/page2.js
@@ -1,21 +1,18 @@
 document.addEventListener("DOMContentLoaded", function () {
-  // Hämta URL-parametrar
   var params = new URLSearchParams(window.location.search);
 
-  // Hämta namnet på den klickade filmen från URL-parametrarna
   var movieName = params.get("name");
 
-  // Loopa igenom arrayen av filmer för att hitta den matchande filmen
   for (var i = 0; i < movies.length; i++) {
     if (movies[i].name === movieName) {
-      // Visa information om den matchande filmen
       document.getElementById("movie-name").textContent = movies[i].name;
       document.getElementById("movie-rating").textContent = movies[i].rating;
       document.getElementById("movie-actors").textContent =
         movies[i].actors.join(", ");
       document.getElementById("movie-description").textContent =
         movies[i].description;
-      break; // Avsluta loopen när den matchande filmen har hittats
+      document.getElementById("movie-image").src = movies[i].image;
+      break;
     }
   }
 });
